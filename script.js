cb6e9a8edd3cab05e5880c8da346cb61cc59a775
Caleb Shade <cs021@lvc.edu>
2023-03-30 21:28:21 -0400

    base commit

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollToPlugin,ScrollTrigger);
+
+async function getData(url = "") {
+    const response = await fetch(url, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    return response.json();
+  };
+function countTo(viewCount) {
+    let from = 0
+    let to = viewCount;
+    let step = to > from ? 462960 : -462960;
+    let interval = 3;
+
+    if(from >= to){
+        document.querySelector("#views").textContent = from.toLocaleString("en-US");
+        return;
+    }
+
+    let counter = setInterval(function(){
+        from += step;
+        document.querySelector("#views").textContent = from.toLocaleString("en-US");
+
+        if(from >= to){
+            clearInterval(counter);
+        }
+    }, interval);
+};
+getData("https://app.ytjobs.co/api/talents/1040").then((data) => {
+    var totalViewCount = data.youtubeVideos.statistics.views;
+    var totalVideoCount = data.youtubeVideos.statistics.counts;
+    document.querySelector("#videoCount").textContent = totalVideoCount;
+    countTo(totalViewCount);
+});
+
